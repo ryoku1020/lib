ab15835293e177642ffcc5a1e2a8e5e883da4875
ryoku1020 <13takutaku1313@gmail.com>
2026-03-26 19:05:43 +0900

    update

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include"base.cpp"
 template<class G>
-vvc<int>scc(const G& g){
+vc<int>scc(const G& g){
     assert(G::directed()==true);
     int n=g.size();
     vc<int>ord,v1(n),v2(n);
@@ -10,11 +10,12 @@ vvc<int>scc(const G& g){
         ord.pb(u);
     };
     rep(i,n)if(!v1[i])dfs(dfs,i);
-    reverse(all(ord));vvc<int>res;
+    reverse(all(ord));vc<int>res(n);
+    int cnt=0;
     auto dfs2=[&](auto&dfs2,int u)->void{
-        v2[u]=1;res.back().pb(u);
+        v2[u]=1;res[u]=cnt;
         for(auto&e:g.inv(u))if(!v2[e.to])dfs2(dfs2,e.to);
     };
-    for(int x:ord)if(!v2[x]){res.pb({});dfs2(dfs2,x);}
+    for(int x:ord)if(!v2[x]){dfs2(dfs2,x);++cnt;}
     return res;
 }
