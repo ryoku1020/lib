cd6f24e0bbf5c3b7f034fe05274834bd1bfb3304
ryoku1020 <13takutaku1313@gmail.com>
2026-03-30 06:58:10 +0900

    update

--- math/xor_convolution.hpp
+++ math/xor_convolution.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include"../template.hpp"
+#include"fps.hpp"
 template<class mint>
-void fwht(vc<mint>&a){
+void fwht(vc<mint>&a,bool is_inv){
     assert(mint::get_mod());
     int n=a.size();
     for(int h=1;h<n;h*=2){
@@ -14,6 +15,10 @@ void fwht(vc<mint>&a){
             }
         }
     }
+    if(is_inv){
+        mint ih=1/mint(a.size());
+        for(auto&x:a)x*=ih;
+    }
 }
 template<class mint>
 vc<mint>xor_convolution(vc<mint>a,vc<mint>b){
@@ -21,11 +26,9 @@ vc<mint>xor_convolution(vc<mint>a,vc<mint>b){
     int lg=0;while((1<<lg)<N)lg++;
     a.resize(1<<lg);
     b.resize(1<<lg);
-    fwht(a);
-    fwht(b);
+    fwht(a,0);
+    fwht(b,0);
     rep(i,1<<lg)a[i]*=b[i];
-    fwht(a);
-    mint iv=1/mint(2).pow(lg);
-    rep(i,1<<lg)a[i]*=iv;
+    fwht(a,1);
     return a;
 }
