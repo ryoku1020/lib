5b16a813bbf065618c5cd9dbeb5092759066bf65
ryoku1020 <13takutaku1313@gmail.com>
2026-04-11 17:59:35 +0900

    upd

---
+++ math/enumerate_floor.hpp
@@ -0,0 +1,11 @@
+#pragma  once
+#include"../template.hpp"
+vc<ll>floors(ll n){
+    assert(n>=0);
+    if(n==0)return {};
+    vc<ll>res;
+    ll m=isqrt(n);
+    REP(i,1,m+1)res.pb(i);
+    DREP(i,m,1)if(n/i>m)res.pb(n/i);
+    return res;
+}
---
+++ math/prime_counting.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include"../template.hpp"
+#include"enumerate_floor.hpp"
+ll prime_counting(ll n){
+    auto QN=floors(n);
+    vc<ll>dp(QN.size()); 
+    rep(i,QN.size())dp[i]=QN[i];
+    int m_val=isqrt(n);
+    vector<int>id1(m_val+1),id2(m_val+1);
+    for(int i=0;i<QN.size();++i){
+        if(QN[i]<=m_val)id1[QN[i]]=i;
+        else id2[n/QN[i]]=i;
+    }
+    auto get_idx=[&](ll x){return x<=m_val?id1[x]:id2[n/x];};
+    vc<int>primes;
+    for(ll x=2;x<=m_val;x++){
+        if(dp[x-1]>dp[x-2]){
+            primes.pb(x);
+            for(int i=(int)QN.size()-1;i>=0;--i){
+                auto&v=QN[i];
+                if(x*x>v)break;
+                dp[i]-=(dp[get_idx(v/x)]-dp[x-2]);
+            }
+        }
+    }
+    return dp.back()-1;
+}
